fa5c5ddb22948722c03d3056b89b1587f4b21ef1
22p26c0377 <pawut.j@gmail.com>
2021-08-31 04:52:03 +0800

    Add graphScatter Data

--- src/App.js
+++ src/App.js
@@ -7,34 +7,8 @@ import { useState } from "react";
 import YieldComponent from "./component/YieldComponent";
 
 import { colors } from "./colors";
-const data = {
-  datasets: [
-    {
-      data: [
-        {
-          x: 1,
-          y: 1,
-        },
-        {
-          x: 0.8,
-          y: 0.8,
-        },
-        {
-          x: 0.4,
-          y: 0.4,
-        },
-        {
-          x: 0.2,
-          y: 0.2,
-        },
-      ],
-      backgroundColor: ["red", "red", "green", "green"],
-    },
-  ],
-};
 
 function App() {
-  const [dataState, setDataState] = useState(data);
   return (
     <div className="App" style={{ backgroundColor: colors.background }}>
       <div class="container">
@@ -51,7 +25,7 @@ function App() {
           </div>
           <div class="col-lg-4">
             <div style={{ backgroundColor: "#2B3340", borderRadius: "10px" }}>
-              <ScatterComponent data={dataState} />
+              <ScatterComponent />
             </div>
           </div>
         </div>
--- src/component/ScatterComponent.js
+++ src/component/ScatterComponent.js
@@ -1,33 +1,58 @@
 import React from "react";
 import { Bar, Scatter } from "react-chartjs-2";
+import scatterData from "../scatterData.json";
 
-const dataMock = {
+const getArrayFromObj = (obj) => Object.keys(obj).map((key) => obj[key]);
+const data = {
   datasets: [
     {
       data: [
         {
-          x: -10,
-          y: 0,
+          x: 1,
+          y: 1,
         },
         {
-          x: 0,
-          y: 10,
+          x: 0.8,
+          y: 0.8,
         },
         {
-          x: 10,
-          y: 5,
+          x: 0.4,
+          y: 0.4,
         },
         {
-          x: 0.5,
-          y: 5.5,
+          x: 0.2,
+          y: 0.2,
         },
       ],
-      backgroundColor: "rgb(255, 99, 132)",
+      backgroundColor: "red",
     },
   ],
 };
 
-function ScatterComponent({ data }) {
+const zip = (x, y) => {
+  let t = [];
+  for (let i = 0; i < x.length; i++) {
+    t = [...t, { x: x[i], y: y[i] }];
+  }
+  return t;
+};
+
+const createDataChart = (data) => {
+  const _x = getArrayFromObj(scatterData.cumulative_cycletime);
+  const _y = getArrayFromObj(scatterData.cumulative_stdtime);
+  const zipData = zip(_x, _y);
+  return {
+    datasets: [
+      {
+        data: zipData,
+        backgroundColor: "red",
+      },
+    ],
+  };
+};
+
+function ScatterComponent({}) {
+  const data = createDataChart(scatterData);
   return (
     <div>
       <Scatter
