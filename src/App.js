c5c0eacbb7abe025359c7a739906be77ad6b7db6
22p26c0377 <pawut.j@gmail.com>
2021-08-31 05:28:22 +0800

    restructer asset

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import ScatterComponent from "./component/ScatterComponent";
 import BarComponent from "./component/BarComponent";
 import { useState } from "react";
 import YieldComponent from "./component/YieldComponent";
-
+import StatusTable from "./component/StatusTable";
+import TableRow from "./component/StatusTableRow";
 import { colors } from "./colors";
-
+// const tablecon1 = "/asset/tableIcon1.svg";
 function App() {
   return (
     <div className="App" style={{ backgroundColor: colors.background }}>
@@ -22,7 +23,30 @@ function App() {
             <div style={{ backgroundColor: "#2B3340", borderRadius: "10px" }}>
               <YieldComponent />
             </div>
-            <div></div>
+            <div>
+              <StatusTable>
+                <TableRow
+                  icon={""}
+                  found="พบการไม่ใส่หน้ากากอนามัยขณะทำงาน"
+                  status="status"
+                  time="13.27 PM"
+                  date="July 21,2021"
+                ></TableRow>
+                <TableRow
+                  found="พบการไม่ใส่หน้ากากอนามัยขณะทำงาน"
+                  status="status"
+                  time="13.27 PM"
+                  date="July 21,2021"
+                ></TableRow>
+
+                <TableRow
+                  found="พบการไม่ใส่หน้ากากอนามัยขณะทำงาน"
+                  status="status"
+                  time="13.27 PM"
+                  date="July 21,2021"
+                ></TableRow>
+              </StatusTable>
+            </div>
           </div>
           <div class="col-lg-4">
             <p className="text-start text-white mb-0">{`STD Time & Current`}</p>
---
+++ src/component/StatusCardComponent.js
@@ -0,0 +1,27 @@
+import "../App.css";
+import { useState } from "react";
+import Logo from "./logo.svg";
+
+function StatusCardComponent({ Logo, h1txt, color, percent }) {
+  return (
+    <div className="cardBG statusCard row">
+      <div class="col-sm-3 statusCardItem">
+        <img src={Logo} />
+      </div>
+      <div class="col-sm-9 statusCardItem">
+        <h1>{h1txt}</h1>
+        <p className="subtxt">
+          <span
+            className={color === "g" ? "colorGreen" : "colorRed"}
+            style={{ marginRight: "0.75em" }}
+          >
+            {percent}
+          </span>
+          since lastweek
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default StatusCardComponent;
