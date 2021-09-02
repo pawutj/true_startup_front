ecd06388f7b9c76869e985847e24acf8015adf36
22p26c0377 <pawut.j@gmail.com>
2021-09-03 00:47:20 +0800

    fix Text

--- src/App.js
+++ src/App.js
@@ -45,20 +45,20 @@ function App() {
                     h1txt="3.91 HOUR"
                     percent="4.02%"
                     color="g"
-                    Logo="./asset/iconimg1.svg"
+                    Logo="./asset/iconimg2.svg"
                   />
                 </div>
                 <div className="col m-2">
                   <p className="text-start text-white mb-0 mt-2">BREAK</p>
                   <StatusCardComponent
-                    h1txt="1.56 HOUR"
+                    h1txt="3.52 HOUR"
                     percent="+3.96%"
                     color="g"
-                    Logo="./asset/iconimg2.svg"
+                    Logo="./asset/iconimg1.svg"
                   />
                 </div>
                 <div className="col m-2">
-                  <p className="text-start text-white mb-0 mt-2">TIME</p>
+                  <p className="text-start text-white mb-0 mt-2">LOSS</p>
 
                   <StatusCardComponent
                     h1txt="3.91 HOUR"
